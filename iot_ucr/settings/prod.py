8fe45db44a8ca7a499579572700783d23015208f
GaboUCR <gabrielgamboav19@gmail.com>
2025-06-19 19:32:00 -0600

    Added infraestructure
    Signed-off-by: GaboUCR <gabrielgamboav19@gmail.com>

--- iot_ucr/settings/prod.py
+++ iot_ucr/settings/prod.py
@@ -3,11 +3,23 @@ from .base import *
 DEBUG = False
 
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# Para que Django detecte que detrás del proxy la petición fue HTTPS
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+
+# Para que Django respete el Host que llega en el header
+USE_X_FORWARDED_HOST = True
+
+# Debes incluir aquí tu dominio con esquema, para que pase el check de CSRF
+CSRF_TRUSTED_ORIGINS = [
+    "https://iotest.gabrielgamboavargas.site",
+    # agrega aquí más orígenes si usas otros subdominios…
+]
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["iotest.gabrielgamboavargas.site", "gabrielgamboavargas.site"]
 # # Seguridad para producción
 SECURE_HSTS_SECONDS = 3600
 SECURE_SSL_REDIRECT = True
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
+# SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
